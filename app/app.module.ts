ace0c595a2e2f734ea2cc8d4844d8c9bd30975f1
sravanthi velaga <svelaga0412@gmail.com>
2017-12-08 13:16:11 -0800

    add basic index.html and .ts files

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { FormsModule }  from '@angular/forms';
+import { CompletenessPipe } from './completeness.pipe';
+
+@NgModule({
+  imports: [ BrowserModule,
+                  FormsModule ],
+  declarations: [ AppComponent , CompletenessPipe],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
